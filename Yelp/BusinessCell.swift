dbc23dac460b73bff2573fecdfceeadaa893c9f3
Andy Jiang <andyjiang@gmail.com>
2016-10-27 13:40:38 -0700

    updated podfile.lock

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Andy Jiang on 10/26/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
